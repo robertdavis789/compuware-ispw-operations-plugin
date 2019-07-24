9f3ca9045c21840d85a7f97466ada5ae15e7c05e
zhouqr2000 <zhouqr2000@gmail.com>
2019-07-24 15:56:06 -0400

    update constants

--- src/main/java/com/compuware/ispw/git/GitToIspwConstants.java
+++ src/main/java/com/compuware/ispw/git/GitToIspwConstants.java
@@ -2,15 +2,15 @@ package com.compuware.ispw.git;
 
 public class GitToIspwConstants
 {
-	public static String FILE_QUEUE = "file_queue.txt";
+	public static final String FILE_QUEUE = "file_queue.txt";
 	
-	public static String VAR_REF_ID = "refId"; //$NON-NLS-1$
-	public static String VAR_REF = "ref"; //$NON-NLS-1$
-	public static String VAR_HASH = "hash"; //$NON-NLS-1$
+	public static final String VAR_REF_ID = "refId"; //$NON-NLS-1$
+	public static final String VAR_REF = "ref"; //$NON-NLS-1$
+	public static final String VAR_HASH = "hash"; //$NON-NLS-1$
 
-	public static String CONTAINER_PREF_PER_COMMIT = "per-commit"; //$NON-NLS-1$
-	public static String CONTAINER_PREF_PER_BRANCH = "per-branch"; //$NON-NLS-1$
-	public static String CONTAINER_PREF_CUSTOM = "custom"; //$NON-NLS-1$
+	public static final String CONTAINER_PREF_PER_COMMIT = "per-commit"; //$NON-NLS-1$
+	public static final String CONTAINER_PREF_PER_BRANCH = "per-branch"; //$NON-NLS-1$
+	public static final String CONTAINER_PREF_CUSTOM = "custom"; //$NON-NLS-1$
 
 	public static final String SCM_DOWNLOADER_CLI_BAT = "IspwCLI.bat"; //$NON-NLS-1$
 	public static final String SCM_DOWNLOADER_CLI_SH = "IspwCLI.sh"; //$NON-NLS-1$
@@ -28,7 +28,7 @@ public class GitToIspwConstants
 	public static final String GIT_REF_PARAM = "-gitBranch"; //$NON-NLS-1$
 	public static final String GIT_HASH_PARAM = "-gitCommit"; //$NON-NLS-1$
 	
-	public static String BRANCH_MAPPING_DEFAULT = "#The following comments show how to use the 'Branch Mapping' field.\n"
+	public static final String BRANCH_MAPPING_DEFAULT = "#The following comments show how to use the 'Branch Mapping' field.\n"
 			+ "#Click on the help button to the right of the screen for more details on how to populate this field\n"
 			+ "#\n" + "#**/dev1/ => DEV1, per-commit\n" + "#**/dev2/ => DEV2, per-branch\n"
 			+ "#**/dev3/ => DEV3, custom, a description\n";
